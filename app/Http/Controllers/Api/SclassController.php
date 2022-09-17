9450da27e6bbfc7341d6a1ab17057d278ae9572c
Bhavitha <bhavithaseva@gmail.com>
2022-09-17 12:47:06 +0530

    214 to 217 restapi to MCR,first route and method

---
+++ app/Http/Controllers/Api/SclassController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Sclass;
+
+class SclassController extends Controller
+{
+    public  function Index(){
+        $sclass =Sclass::latest()->get();
+        return response()->json($sclass);
+    }
+
+    public function Store(Request $request){
+
+        $validatedData = $request->validate([
+            'class_name' => 'required|unique:sclasses|max:255',
+         
+        ]);
+
+      
+        Sclass::insert([
+            'class_name' =>$request->class_name,
+        ]);
+        return response('Student Class inserted successfully');
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\SclassController;
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/class',[SclassController::class, 'Index']);
+Route::post('/class/store',[SclassController::class, 'Store']);
