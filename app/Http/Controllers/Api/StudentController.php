f111b51b5a4dd1cd815fd6f38251c4a3db27e2e5
Bhavitha <bhavithaseva@gmail.com>
2022-09-17 16:48:40 +0530

    225. RestAPI section CRUD Part 2

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Student;
@@ -37,6 +36,37 @@ class StudentController extends Controller
             
         ]);
         return response('Student  inserted successfully');
+
+    }
+    public function Edit($id){
+      
+        $Student =Student::findOrFail($id);
+        return response()->json($Student);
+
+    }
+
+    public function Update(Request $request, $id){
+
+        Student::findOrFail($id)->update([
+           'class_id' =>$request->class_id,
+            'section_id' =>$request->section_id,
+            'name' =>$request->name,
+            'address' =>$request->address,
+            'phone' =>$request->phone,
+            'email' =>$request->email,
+            'password' =>Hash::make($request->password),
+            'photo' =>$request->photo,
+            'gender' =>$request->gender,
+            'created_at'=> Carbon::now()
+         
+        ]);
+        return response(' Student Updated successfully');
+
+    }
+    public function Delete($id){
+
+        Student::findOrFail($id)->delete();
+        return response('Student Deleted successfully');
 
     }
 
