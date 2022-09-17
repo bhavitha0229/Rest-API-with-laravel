a90877e1055381c872f58f18799d52a6b891ca48
Bhavitha <bhavithaseva@gmail.com>
2022-09-17 14:32:22 +0530

    219. RestAPI Class CRUD Part 2

--- app/Http/Controllers/Api/SclassController.php
+++ app/Http/Controllers/Api/SclassController.php
@@ -33,4 +33,21 @@ class SclassController extends Controller
         return response()->json($sclass);
 
     }
+
+    public function Update(Request $request, $id){
+
+        Sclass::findOrFail($id)->update([
+            'class_name' => $request->class_name,
+         
+        ]);
+        return response('Student Class Updated successfully');
+
+    }
+    public function Delete($id){
+
+        Sclass::findOrFail($id)->delete();
+        return response('Student Class Deleted successfully');
+
+    }
+
 }
